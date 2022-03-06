eb38d508b96ea225b4e74b82dbceca4f445d69c7
Michael <15317421+ActualMandM@users.noreply.github.com>
2022-03-06 12:31:51 -0800

    SORRY OOPS

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -200,14 +200,14 @@ class TitleState extends MusicBeatState
 		#elseif CHARTING
 		MusicBeatState.switchState(new ChartingState());
 		#else
-		if (!FlxG.save.data.extra2beaten || FlxG.save.data.extra2beaten == null)
-		{
-			FlxTransitionableState.skipNextTransIn = true;
-			FlxTransitionableState.skipNextTransOut = true;
-			MusicBeatState.switchState(new FlashingState());
-		}
-		else
-		{
+		// if (!FlxG.save.data.extra2beaten || FlxG.save.data.extra2beaten == null)
+		// {
+		// 	FlxTransitionableState.skipNextTransIn = true;
+		// 	FlxTransitionableState.skipNextTransOut = true;
+		// 	MusicBeatState.switchState(new FlashingState());
+		// }
+		// else
+		// {
 			#if desktop
 			DiscordClient.initialize();
 			Application.current.onExit.add(function(exitCode)
@@ -219,7 +219,7 @@ class TitleState extends MusicBeatState
 			{
 				startIntro();
 			});
-		}
+		// }
 		#end
 	}
 
