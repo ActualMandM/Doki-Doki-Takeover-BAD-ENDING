78d65a8ab78f45b8b007d7674672d4c69e83f891
Michael <15317421+ActualMandM@users.noreply.github.com>
2022-06-28 23:54:39 -0700

    actually hide all of these if it's not beaten yet

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -66,30 +66,30 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 			option.displayFormat = '%vX';
 			optionsArray.push(option); */
 
-		var option:GameplayOption = new GameplayOption('Health Gain Multiplier', 'healthgain', 'float', 1);
-		option.scrollSpeed = 2.5;
-		option.minValue = 0;
-		option.maxValue = 5;
-		option.changeValue = 0.1;
-		option.displayFormat = '%vX';
-		optionsArray.push(option);
-
-		var option:GameplayOption = new GameplayOption('Health Loss Multiplier', 'healthloss', 'float', 1);
-		option.scrollSpeed = 2.5;
-		option.minValue = 0.5;
-		option.maxValue = 5;
-		option.changeValue = 0.1;
-		option.displayFormat = '%vX';
-		optionsArray.push(option);
-
-		var option:GameplayOption = new GameplayOption('Instakill on Miss', 'instakill', 'bool', false);
-		optionsArray.push(option);
-
-		var option:GameplayOption = new GameplayOption('Practice Mode', 'practice', 'bool', false);
-		optionsArray.push(option);
-
 		if (ClientPrefs.storycomplete)
 		{
+			var option:GameplayOption = new GameplayOption('Health Gain Multiplier', 'healthgain', 'float', 1);
+			option.scrollSpeed = 2.5;
+			option.minValue = 0;
+			option.maxValue = 5;
+			option.changeValue = 0.1;
+			option.displayFormat = '%vX';
+			optionsArray.push(option);
+
+			var option:GameplayOption = new GameplayOption('Health Loss Multiplier', 'healthloss', 'float', 1);
+			option.scrollSpeed = 2.5;
+			option.minValue = 0.5;
+			option.maxValue = 5;
+			option.changeValue = 0.1;
+			option.displayFormat = '%vX';
+			optionsArray.push(option);
+
+			var option:GameplayOption = new GameplayOption('Instakill on Miss', 'instakill', 'bool', false);
+			optionsArray.push(option);
+
+			var option:GameplayOption = new GameplayOption('Practice Mode', 'practice', 'bool', false);
+			optionsArray.push(option);
+
 			var option:GameplayOption = new GameplayOption('Botplay', 'botplay', 'bool', false);
 			optionsArray.push(option);
 		}
